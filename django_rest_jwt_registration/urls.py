178381ed5d1910b3f4358713d2038c9fdfd8a3c1
János Gönye <janos.gonye@gmail.com>
2021-03-13 13:13:48 +0100

    register the base API endpoint in 'demo.urls'

---
+++ django_rest_jwt_registration/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django_rest_jwt_registration import views
+
+
+urlpatterns = [
+    path('registration/', views.RegistrationAPIView.as_view(), name='registration'),
+    path('registration/confirm', views.RegistrationConfirmAPIView.as_view(), name='registration_confirm'),
+    path('registration/delete', views.RegistrationDeleteAPIView.as_view(), name='registration_delete'),
+    path('registration/delete/confirm', views.RegistrationConfirmDeleteAPIView.as_view(), name='registration_delete_confirm'),
+]
--- django_rest_jwt_registration/views.py
+++ django_rest_jwt_registration/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class RegistrationAPIView(APIView):
+    permission_classes = ()
+
+    def post(self, request):
+        return Response({'hello': 'world!'})
+
+
+class RegistrationConfirmAPIView(APIView):
+    permission_classes = ()
+
+    def get(self, request):
+        return Response({'hello': 'world!'})
+
+
+class RegistrationDeleteAPIView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        return get_user_model().objects.get(pk=self.request.user.id)
+
+    def post(self, request):
+        return Response({'hello': 'world!'})
+
+
+class RegistrationConfirmDeleteAPIView(APIView):
+    permission_classes = ()
+
+    def get(self, request):
+        return Response({'hello': 'world!'})
