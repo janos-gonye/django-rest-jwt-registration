2eaabb963722908670eeb228ba456e2e8c1cea12
János Gönye <janos.gonye@gmail.com>
2021-03-14 21:30:29 +0100

    add 'django_rest_jwt_registration.exceptions'

---
+++ django_rest_jwt_registration/exceptions.py
@@ -0,0 +1,13 @@
+from django.utils.translation import ugettext_lazy as _
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+
+class BadRequestError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Bad request error')
+
+
+class InternalServerError(APIException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = _('Internal server error')
