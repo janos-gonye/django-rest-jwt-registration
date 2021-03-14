65246b4f20b094b2df47970a26cce90adb4aa5f9
János Gönye <janos.gonye@gmail.com>
2021-03-14 23:52:30 +0100

    add restframework urls to 'demo.urls.urlpatterns'

--- demo/urls.py
+++ demo/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('account/', include('django_rest_jwt_registration.urls')),
 ]
