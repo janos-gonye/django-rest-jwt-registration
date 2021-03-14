df3620a9e5d5135fa36e7ea6583945ff176327e9
János Gönye <janos.gonye@gmail.com>
2021-03-14 22:57:33 +0100

    add ending slashes to the urls

--- django_rest_jwt_registration/urls.py
+++ django_rest_jwt_registration/urls.py
@@ -4,7 +4,7 @@ from django_rest_jwt_registration import views
 
 urlpatterns = [
     path('registration/', views.RegistrationAPIView.as_view(), name='registration'),
-    path('registration/confirm', views.RegistrationConfirmAPIView.as_view(), name='registration_confirm'),
-    path('registration/delete', views.RegistrationDeleteAPIView.as_view(), name='registration_delete'),
-    path('registration/delete/confirm', views.RegistrationConfirmDeleteAPIView.as_view(), name='registration_delete_confirm'),
+    path('registration/confirm/', views.RegistrationConfirmAPIView.as_view(), name='registration_confirm'),
+    path('registration/delete/', views.RegistrationDeleteAPIView.as_view(), name='registration_delete'),
+    path('registration/delete/confirm/', views.RegistrationConfirmDeleteAPIView.as_view(), name='registration_delete_confirm'),
 ]
