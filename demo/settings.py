024035e53523b0fa63b4a29fa1daebea9e7a4e42
János Gönye <janos.gonye@gmail.com>
2021-03-24 07:52:26 +0100

    override default djrj templates

--- demo/settings.py
+++ demo/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
