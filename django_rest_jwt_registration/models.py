02df5a8161f313d0bf9b7530cf39abc095977a56
János Gönye <janos.gonye@gmail.com>
2021-04-03 15:20:53 +0200

    add 'type' field to 'Token' model

--- django_rest_jwt_registration/models.py
+++ django_rest_jwt_registration/models.py
@@ -14,5 +14,14 @@ class AbstractEmailRequiredUser(AbstractUser):
 
 
 class Token(models.Model):
+    REGISTRATION_TOKEN = 'rt'
+    REGISTRATION_DELETE_TOKEN = 'rd'
+    PASSWORD_CHANGE_TOKEN = 'pc'
+    TOKEN_TYPES = (
+        (REGISTRATION_TOKEN, _('Registration token')),
+        (REGISTRATION_DELETE_TOKEN, _('Registration Delete Token')),
+        (PASSWORD_CHANGE_TOKEN, _('Password Change Token')),
+    )
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(choices=TOKEN_TYPES, max_length=3, default=REGISTRATION_TOKEN)
