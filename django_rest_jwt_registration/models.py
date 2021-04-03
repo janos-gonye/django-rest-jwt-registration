2fd414f7ab83510d5e55abae518cabf059fbe1c9
János Gönye <janos.gonye@gmail.com>
2021-04-03 15:44:32 +0200

    add properties 'expires_at', 'lifetime' to 'Token' model

--- django_rest_jwt_registration/models.py
+++ django_rest_jwt_registration/models.py
@@ -1,9 +1,16 @@
+import datetime
 import uuid
 
+from django.conf import settings
 from django.core.validators import EmailValidator
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+
+REGISTRATION_TOKEN_LIFETIME = settings.REST_JWT_REGISTRATION['REGISTRATION_TOKEN_LIFETIME']
+REGISTRATION_DELETE_TOKEN_LIFETIME = settings.REST_JWT_REGISTRATION['REGISTRATION_DELETE_TOKEN_LIFETIME']
+PASSWORD_CHANGE_TOKEN_LIFETIME = settings.REST_JWT_REGISTRATION['PASSWORD_CHANGE_TOKEN_LIFETIME']
 
 
 class AbstractEmailRequiredUser(AbstractUser):
@@ -25,3 +32,15 @@ class Token(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     type = models.CharField(choices=TOKEN_TYPES, max_length=3, default=REGISTRATION_TOKEN)
+
+    @property
+    def lifetime(self):
+        return {
+            Token.REGISTRATION_TOKEN: REGISTRATION_TOKEN_LIFETIME,
+            Token.REGISTRATION_DELETE_TOKEN: REGISTRATION_DELETE_TOKEN_LIFETIME,
+            Token.PASSWORD_CHANGE_TOKEN: PASSWORD_CHANGE_TOKEN_LIFETIME,
+        }[self.type]
+
+    @property
+    def expires_at(self):
+        return self.created_at.astimezone(datetime.timezone.utc) + datetime.timedelta(seconds=self.lifetime)
--- django_rest_jwt_registration/token.py
+++ django_rest_jwt_registration/token.py
@@ -8,29 +8,12 @@ from django_rest_jwt_registration.exceptions import TokenDecodeError
 from django_rest_jwt_registration.models import Token
 
 
-REGISTRATION_TOKEN_LIFETIME = settings.REST_JWT_REGISTRATION['REGISTRATION_TOKEN_LIFETIME']
-REGISTRATION_DELETE_TOKEN_LIFETIME = settings.REST_JWT_REGISTRATION['REGISTRATION_DELETE_TOKEN_LIFETIME']
-PASSWORD_CHANGE_TOKEN_LIFETIME = settings.REST_JWT_REGISTRATION['PASSWORD_CHANGE_TOKEN_LIFETIME']
-
-
 def encode_token(payload, token_type):
      # Don't lose reference
     payload = dict(payload)
     token_db_instance = Token.objects.create(type=token_type)
     payload['__id__'] = str(token_db_instance.id)
     return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
-
-
-def _get_lifetime_by_token_type(token_type):
-    try:
-        return {
-            Token.REGISTRATION_TOKEN: REGISTRATION_TOKEN_LIFETIME,
-            Token.REGISTRATION_DELETE_TOKEN: REGISTRATION_DELETE_TOKEN_LIFETIME,
-            Token.PASSWORD_CHANGE_TOKEN: PASSWORD_CHANGE_TOKEN_LIFETIME,
-        }[token_type]
-    except KeyError as err:
-        raise NotImplementedError(
-            f"Token type '{token_type}' not registered in '_get_lifetime_by_token_type'") from err
 
 
 def decode_token(token, token_type):
@@ -45,10 +28,5 @@ def decode_token(token, token_type):
         raise TokenDecodeError(_('Token invalid')) from err
     if token.type != token_type:
         raise TokenDecodeError(_('Token invalid'))
-    lifetime = _get_lifetime_by_token_type(token_type)
-    now = datetime.datetime.now().astimezone(datetime.timezone.utc)
-    expired_at = token.created_at.astimezone(datetime.timezone.utc) + datetime.timedelta(seconds=lifetime)
-    if now > expired_at:
-        raise TokenDecodeError(_('Token expired'))
     del payload['__id__']
     return payload
