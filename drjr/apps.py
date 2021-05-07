cfd413d356d135433c9b8973108ad01e2348cb70
János Gönye <janos.gonye@gmail.com>
2021-05-07 22:21:50 +0200

    use serializer to simple validated app settings

--- drjr/apps.py
+++ drjr/apps.py
@@ -1,96 +1,46 @@
 from django.apps import AppConfig
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from drjr.utils import import_elm_from_str
+
+
+class AppSettingsSerializer(serializers.Serializer):
+    CREATE_USER_SERIALIZER = serializers.CharField(required=False)
+    REGISTRATION_TOKEN_LIFETIME = serializers.IntegerField(required=False, min_value=1, default=3600)
+    REGISTRATION_DELETE_TOKEN_LIFETIME = serializers.IntegerField(required=False, min_value=1, default=3600)
+    PASSWORD_CHANGE_TOKEN_LIFETIME = serializers.IntegerField(required=False, min_value=1, default=3600)
+    EMAIL_CHANGE_TOKEN_LIFETIME = serializers.IntegerField(required=False, min_value=1, default=3600)
+    DELETE_EXPIRED_TOKENS_INTERVAL = serializers.IntegerField(required=False, min_value=1, default=3600)
+
+    def validate_CREATE_USER_SERIALIZER(self, value):
+        try:
+            user_serializer = import_elm_from_str(value)
+            if not issubclass(user_serializer, serializers.ModelSerializer):
+                raise serializers.ValidationError(
+                    "'REST_JWT_REGISTRATION.CREATE_USER_SERIALIZER' is "
+                    f"expected to be a subclass of {serializers.ModelSerializer}")
+        except Exception as err:
+            raise serializers.ValidationError(
+                "'REST_JWT_REGISTRATION.CREATE_USER_SERIALIZER' refers "
+                "to an object that does not exist") from err
+        return value
 
 
 class DjangoRestJwtRegistrationConfig(AppConfig):
     name = 'drjr'
 
-    def __init__(self, *args, **kwargs):
-        self.app_settings = {}
-        super().__init__(*args, **kwargs)
-
     def ready(self):
-        self._check_settings()
-
-    def _check_settings(self):
-        self._validate_app_settings_object()
-        self._validate_create_user_serializer()
-        self._validate_registration_token_lifetime()
-        self._validate_registration_delete_token_lifetime()
-        self._validate_password_change_token_lifetime()
-        self._validate_email_change_token_lifetime()
-        self._validate_delete_expired_tokens_interval()
-
-    def _validate_app_settings_object(self):
         try:
-            self.app_settings = getattr(settings, 'REST_JWT_REGISTRATION')
+            app_settings = getattr(settings, 'REST_JWT_REGISTRATION')
+            if not isinstance(app_settings, dict):
+                raise ImproperlyConfigured(
+                    f"'REST_JWT_REGISTRATION' is expected to be of type {dict} "
+                    f"or a subclass of {dict}")
+            serializer = AppSettingsSerializer(data=app_settings)
+            serializer.is_valid(raise_exception=True)
         except AttributeError as err:
             raise ImproperlyConfigured("'REST_JWT_REGISTRATION' is not set") from err
-        if not isinstance(self.app_settings, dict):
-            raise ImproperlyConfigured(
-                f"'REST_JWT_REGISTRATION' is expected to be of type {dict} or a subclass of {dict}")
-
-    def _validate_create_user_serializer(self):
-        try:
-            user_serializer = self.app_settings['CREATE_USER_SERIALIZER']
-        except KeyError as err:
-            # Use default in 'views.py'
-            return
-        if not isinstance(user_serializer, str):
-            raise ImproperlyConfigured(f"'CREATE_USER_SERIALIZER' is expected to be of type {str}")
-
-        try:
-            user_serializer = import_elm_from_str(user_serializer)
-        except Exception as err:
-            raise ImproperlyConfigured(
-                "'REST_JWT_REGISTRATION.CREATE_USER_SERIALIZER' refers to an object that does not exist") from err
-
-        try:
-            if not issubclass(user_serializer, ModelSerializer):
-                raise ValueError
-        except ValueError as err:
-            raise ImproperlyConfigured(
-                "'REST_JWT_REGISTRATION.CREATE_USER_SERIALIZER' is "
-                f"expected to be a subclass of {ModelSerializer}") from err
-
-    def _validate_token_lifetime(self, name):
-        try:
-            value = self.app_settings[name]
-        except KeyError as err:
-            raise ImproperlyConfigured(
-                f"'REST_JWT_REGISTRATION' is missing key '{name}'") from err
-        if not isinstance(value, int):
-            raise ImproperlyConfigured(
-                f"'REST_JWT_REGISTRATION.{name}' is not of type {int}")
-
-        if value < 0:
-            raise ImproperlyConfigured(f"'{name}' must be greater than 0")
-
-    def _validate_registration_token_lifetime(self):
-        self._validate_token_lifetime(name='REGISTRATION_TOKEN_LIFETIME')
-
-    def _validate_registration_delete_token_lifetime(self):
-        self._validate_token_lifetime(name='REGISTRATION_DELETE_TOKEN_LIFETIME')
-
-    def _validate_password_change_token_lifetime(self):
-        self._validate_token_lifetime(name='PASSWORD_CHANGE_TOKEN_LIFETIME')
-
-    def _validate_email_change_token_lifetime(self):
-        self._validate_token_lifetime(name='EMAIL_CHANGE_TOKEN_LIFETIME')
-
-    def _validate_delete_expired_tokens_interval(self):
-        try:
-            value = self.app_settings['DELETE_EXPIRED_TOKENS_INTERVAL']
-        except KeyError as err:
-            raise ImproperlyConfigured(
-                "'REST_JWT_REGISTRATION' is missing key 'DELETE_EXPIRED_TOKENS_INTERVAL'") from err
-        if not isinstance(value, int):
-            raise ImproperlyConfigured(
-                f"'REST_JWT_REGISTRATION.DELETE_EXPIRED_TOKENS_INTERVAL' is not of type {int}")
-
-        if value < 0:
-            raise ImproperlyConfigured("'DELETE_EXPIRED_TOKENS_INTERVAL' must be greater than 0")
+        except serializers.ValidationError as err:
+            raise ImproperlyConfigured(str(err)) from err
