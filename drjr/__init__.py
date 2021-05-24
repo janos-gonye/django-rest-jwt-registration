131e910bf542405850c67f5741cba3fc3ef7bcd2
János Gönye <janos.gonye@gmail.com>
2021-05-24 14:46:49 +0200

    add '__version__' and '__author__' in '__init__'

--- drjr/__init__.py
+++ drjr/__init__.py
@@ -1,1 +1,10 @@
-default_app_config = 'drjr.apps.DjangoRestJwtRegistrationConfig'
+"""
+django-rest-jwt-registration
+
+Django app for registration password reset, email -
+and password change with jwt tokens and email sending
+"""
+
+
+__version__ = "0.1.0"
+__author__ = "János Gönye"
