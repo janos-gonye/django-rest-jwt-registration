d1e5d7d4b3e16f703b3aacd951510d04f0454001
János Gönye <janos.gonye@gmail.com>
2021-09-26 00:09:24 +0200

    fix 'setup.cfg' and 'setup.py' | include 'templates' directory recursively

--- setup.py
+++ setup.py
@@ -1,42 +1,4 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name='django-rest-jwt-registration',
-    author='János Gönye',
-    author_email='janos.gonye@gmail.com',
-    version='0.1.0',
-    description='Django app for registration password reset, email - and password change with jwt tokens and email sending',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/janos-gonye/django-rest-jwt-registration',
-    license='MIT',
-    packages=[
-        'drjr',
-        'drjr.migrations',
-        'drjr.templates',
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alfa',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    python_requires=">=3.6",
-    setup_requires = [
-        'setuptools >= 38.3.0',
-    ],
-    install_requires=[
-        'Django',
-        'djangorestframework',
-        'pyjwt',
-    ],
-)
+setup()
